5053c72073d273a02fe706e797b8604fbbeb4144
JoaoGabeee <joao2008gabe@gmail.com>
2025-01-08 22:22:45 -0300

    Criação de um controller teste, e inicio da pagina home

--- Projetos Java/agenda/src/test/java/com/joao/agenda/AgendaApplicationTests.java
+++ Projetos Java/agenda/src/test/java/com/joao/agenda/AgendaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.joao.agenda;
 
+import com.joao.agenda.controller.TesteController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {TesteController.class})
 class AgendaApplicationTests {
 
 	@Test
