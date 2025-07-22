f71096e7c3df1126dc5e4cfaf054a1b5dc131109
JoaoGabeee <joao2008gabe@gmail.com>
2025-07-22 19:53:26 -0300

    adiçao de código

--- Projetos Java/organizador-doces/organizador-doces/src/main/java/com/isaejoao/organizador_doces/OrganizadorDocesApplication.java
+++ Projetos Java/organizador-doces/organizador-doces/src/main/java/com/isaejoao/organizador_doces/OrganizadorDocesApplication.java
@@ -2,12 +2,29 @@ package com.isaejoao.organizador_doces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class OrganizadorDocesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(OrganizadorDocesApplication.class, args);
+
+		String url = "http://localhost:8080";
+
+		Runtime runTime = Runtime.getRuntime();
+		runTime.exec(new String[]{"powershell.exe", "-Command", "Start-process", url});
+
 	}
 
+	@GetMapping({"/", "/index", "/index.html"})
+	public String index() {
+
+		return "index";
+	}
 }
+
+
+
--- Projetos Java/organizador-doces/organizador-doces/src/main/java/com/isaejoao/organizador_doces/controller/PrimeiroController.java
+++ Projetos Java/organizador-doces/organizador-doces/src/main/java/com/isaejoao/organizador_doces/controller/PrimeiroController.java
@@ -1,4 +1,14 @@
 package com.isaejoao.organizador_doces.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PrimeiroController {
+    @GetMapping({"/", "/index", "/index.html"})
+    public String index() {
+
+        return "index";
+    }
+
 }
